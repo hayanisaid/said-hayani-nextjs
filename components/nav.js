7b42124a4c58f0ac3cf29ebe4bf935ae47773b4d
said <info.said.dev@gmail.com>
2018-09-16 21:02:09 +0100

    add navigator style

--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,46 @@
 import React from "react";
 import Link from "next/link";
 
-const links = [
-  { href: "https://github.com/segmentio/create-next-app", label: "Github" }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
+// const links = [
+//   { href: "https://github.com/segmentio/create-next-app", label: "Github" }
+// ].map(link => {
+//   link.key = `nav-link-${link.href}-${link.label}`;
+//   return link;
+// });
 
 const Nav = props => (
   <div className={props.cName}>
     <nav>
       <ul className="menu">
         <li className="menu-item">
-          <Link to="/">Home</Link>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
         </li>
-        <li className="menu-item">About</li>
-        <li className="menu-item">Blog</li>
-        <li className="menu-item">Contact</li>
+        <li className="menu-item">
+          {" "}
+          <Link href="/">
+            <a>AboutMe</a>
+          </Link>
+        </li>
+        <li className="menu-item">
+          {" "}
+          <Link href="/">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li className="menu-item">
+          {" "}
+          <Link href="/">
+            <a>Stories</a>
+          </Link>
+        </li>
+        <li className="menu-item">
+          {" "}
+          <Link href="/">
+            <a>Contact</a>
+          </Link>
+        </li>
       </ul>
     </nav>
   </div>
