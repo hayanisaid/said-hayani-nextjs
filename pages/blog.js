239aeef346388c1cc6fe3b4f9fd02f7e523dbbdc
said <info.said.dev@gmail.com>
2018-09-21 23:31:43 +0100

    add articles cards

--- pages/blog.js
+++ pages/blog.js
@@ -2,15 +2,58 @@ import React from "react";
 import Nav from "./../components/nav";
 import Head from "./../components/head";
 import Page from "./../components/page";
-import "./../styles/_nav.styl";
+import Card from "./../components/card";
+//const imag = require("./../static/images/me.jpeg");
 
 class Blog extends React.Component {
   render() {
     return (
-      <div className="blog me">
+      <div className="blog ">
         <Page>
           <Head title="blog" />
-          <div>Hello world from blog</div>
+          <div className="blog-wrapper">
+            <div className="header">
+              <div className="description">
+                <h1>My Articles</h1>
+                <h2>
+                  Writting Articles About allows me to explore the hype of
+                  technology
+                </h2>
+              </div>
+            </div>
+            <div className="cards">
+              <Card
+                title="JavaScript write less do more"
+                image="./../static/images/me.jpeg"
+                description="this is the description of my Articles"
+              />
+              <Card
+                title="JavaScript write less do more"
+                image="./../static/images/me.jpeg"
+                description="this is the description of my Articles"
+              />
+              <Card
+                title="JavaScript write less do more"
+                image="./../static/images/me.jpeg"
+                description="this is the description of my Articles"
+              />
+              <Card
+                title="JavaScript write less do more"
+                image="./../static/images/me.jpeg"
+                description="this is the description of my Articles"
+              />
+              <Card
+                title="JavaScript write less do more"
+                image="./../static/images/me.jpeg"
+                description="this is the description of my Articles"
+              />
+              <Card
+                title="JavaScript write less do more"
+                image="./../static/images/me.jpeg"
+                description="this is the description of my Articles"
+              />
+            </div>
+          </div>
         </Page>
       </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import Home from "./home";
+//import Home from "./home";
+const Home = dynamic(import("./home"), {
+  loading: () => <div>.... loading</div>
+});
 
 class App extends React.Component {
   render() {
     return <Home />;
   }
 }
+export default App;
