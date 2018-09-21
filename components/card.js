e09324881eb273037ee2a8040a51d99b5eeaa6f5
said <info.said.dev@gmail.com>
2018-09-22 00:05:53 +0100

    styling[] style the cards

--- components/card.js
+++ components/card.js
@@ -9,9 +9,9 @@ const Card = props => (
       <div className="card-text">
         <h3>{props.title}</h3>
         <p>{props.description}</p>
-      </div>
-      <div className="card-footer">
-        <a href="#">Read n Medium</a>
+        <div className="card-footer">
+          <a href="#">Read n Medium</a>
+        </div>
       </div>
     </div>
   </div>
