a2b57587f275be28c987556c87434047a8a58455
said <info.said.dev@gmail.com>
2018-11-24 14:31:59 +0100

    fixng loading path

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { connect } from "react-redux";
-import Loading from "/components/Loading";
+import Loading from "../components/Loading";
 //import Home from "./home";
 
 import Wrapper from "../components/Wrapper";
