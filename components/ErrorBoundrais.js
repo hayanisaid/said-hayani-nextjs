6169a25114ac5938d72059ba8c20f0fb8911040e
said <info.said.dev@gmail.com>
2018-11-24 14:21:11 +0100

    feat[] add errorboundrais

---
+++ components/ErrorBoundrais.js
@@ -0,0 +1,8 @@
+const ErrorBoundrais = () => (
+  <div className="loading__container">
+    <img src="/static/images/desktop.svg" className="loading__container-img" />
+    <h3>Sorry somthing went Wrong , please try again!</h3>
+  </div>
+);
+
+export default ErrorBoundrais;
